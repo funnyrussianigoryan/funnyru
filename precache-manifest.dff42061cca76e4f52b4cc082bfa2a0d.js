2d43556e5a6a48904ec7b99897a4d9c833da7db6
funnyrussianigoryan <47246349+funnyrussianigoryan@users.noreply.github.com>
2019-07-29 15:41:22 +0700

    Add files via upload

---
+++ precache-manifest.dff42061cca76e4f52b4cc082bfa2a0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a614d4e1137eadb1be0fd473c690f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4fabaa0d0ccb3a819d6",
+    "url": "/static/css/main.b8e3ac60.chunk.css"
+  },
+  {
+    "revision": "68bf979995e579447536",
+    "url": "/static/js/2.6c11366c.chunk.js"
+  },
+  {
+    "revision": "d4fabaa0d0ccb3a819d6",
+    "url": "/static/js/main.09e60765.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
